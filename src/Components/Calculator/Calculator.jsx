0d0f5f1c3841be221001a7e2fc302deb37343b4f
AyazQadri <107315942+AyazQadri@users.noreply.github.com>
2023-01-11 12:16:53 +0500

    create the calculator and implemented apis

--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -12,6 +12,7 @@ import Select from "@mui/material/Select";
 import Chip from "@mui/material/Chip";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import Box from "@mui/material/Box";
+import axios from 'axios';
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -24,12 +25,59 @@ const MenuProps = {
   },
 };
 
+
+
 const Calculator = () => {
   const navigate = useNavigate();
   const [methods, setMethods] = useState();
+  const [result, setResult] = useState()
+  const [number, setNumber] = useState({
+    one: '',
+    two: ''
+
+  })
+  
+  const baseURL = "http://localhost:3000"
+
+  const Endpoints = {
+    addition: "/add/",
+    subtrction: "/subtract/",
+    mutiplication: "/multiply/"
+  }
+
+  
+  const handleTextFieldOnChange = (e) => {
+    const { name, value } = e.target;
+    setNumber({ ...number, [name]: value });
+  }
+
 
   const handleCalculateFunc = () => {
-    console.log('clicked')
+    if (methods == "Addition"){
+      const completeURL = baseURL + Endpoints.addition + number.one + "/" + number.two;
+      axios.get(completeURL).then((response) => {
+        console.log(response, 'api success')
+        setResult(response.data)
+      }).catch((error) => {
+        console.log(error, 'addition api error console')
+      })
+    } else if (methods == "Subtraction"){
+      const completeURL = baseURL + Endpoints.subtrction + number.one + "/" + number.two;
+      axios.get(completeURL).then((response)=> {
+        console.log(response, 'subtratton api response')
+        setResult(response?.data)
+      }).catch((error) => {
+        console.log(error, 'subtraction api error console')
+      })
+    } else if (methods == "Multiplication"){
+      const completeURL = baseURL + Endpoints.mutiplication + number.one + "/" + number.two;
+      axios.get(completeURL).then((response) => {
+        console.log( 'division response'. response)
+        setResult(response?.data)
+      }).catch((error) => {
+        console.log(error, 'multiplication api error console')
+      })
+    }
   }
 
   return (
@@ -51,15 +99,21 @@ const Calculator = () => {
         <div className="number_input_field_div">
           <input
             type="tel"
+            value={number?.one}
+            name="one"
             placeholder="Enter First Number"
             className="number_input_field_css"
+            onChange={(e) => handleTextFieldOnChange(e)}
           />
         </div>
         <div className="second_input_field_div">
           <input
             type="tel"
+            value={number?.two}
+            name="two"
             placeholder="Enter Second Number"
             className="number_input_field_css"
+            onChange={(e) => handleTextFieldOnChange(e)}
           />
         </div>
         <FormControl
@@ -87,6 +141,9 @@ const Calculator = () => {
             <MenuItem value={"Multiplication"}>Multiplication</MenuItem>
           </Select>
         </FormControl>
+       {result && <Box style={{ width: "80%", fontSize: 'large', display: 'flex', justifyContent: 'flex-start' }}>
+        Output is: <span style={{ paddingLeft: '1rem', fontWeight: 600  }}>{result}</span>
+        </Box>}
         <Box
           style={{
             width: "80%",
